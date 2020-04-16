1e76522993e8ba88679eec353475cce1e91e32cb
Fer <fersnped@gmail.com>
2020-04-16 15:52:33 -0500

    Responsive register and validate form expressions

--- src/components/Registro.vue
+++ src/components/Registro.vue
@@ -7,22 +7,37 @@
       </div>
       <p id="bienvenidaText">¡Bienvenido a capitalRoute!</p>
       <div id="textoFields">
-        <label for="correo">Correo electrónico</label>
-        <input type="text" placeholder name="correo" v-model="correo" />
-        <label for="confirmacionCorreo">Confirmar correo electrónico</label>
-        <input type="text" placeholder name="confirmacionCorreo" v-model="confirmacionCorreo" />
-        <label for="password">Contraseña</label>
-        <input type="password" placeholder name="password" v-model="password" />
-        <label for="confirmacionPassword">Confirmar password</label>
-        <input
-          type="password"
-          placeholder
-          name="confirmacionPassword"
-          v-model="confirmacionPassword"
-        />
+        <v-form v-model="valid">
+          <v-container>
+        <v-row no-gutters>
+          <label >Correo electrónico</label>
+        </v-row>
+        <v-row no-gutters>
+          <v-text-field solo="solo" clearable="clearable" dense="dense" :rules="emailRules" required  v-model="correo" />
+        </v-row>
+        <v-row no-gutters>
+          <label >Confirmar correo electrónico</label>
+        </v-row>
+        <v-row no-gutters> 
+          <v-text-field solo="solo" clearable="clearable" dense="dense" :rules="emailMatch" required  v-model="confirmacionCorreo" />
+        </v-row>
+        <v-row no-gutters>
+          <label >Contraseña</label>
+        </v-row>
+        <v-row no-gutters>  
+          <v-text-field solo="solo" clearable="clearable" dense="dense" type="password" :rules="passwordRules" required v-model="password" />
+        </v-row>
+        <v-row no-gutters>
+          <label >Confirmar password</label>
+        </v-row>
+        <v-row no-gutters>  
+          <v-text-field solo="solo" clearable="clearable" dense="dense" type="password" :rules="passwordMatch" required v-model="confirmacionPassword" />
+        </v-row>
+        </v-container>
+        </v-form>
       </div>
       <div id="buttonWrapper">
-        <button id="crearCuentaBoton" @click="crearCuenta()">CREAR CUENTA</button>
+        <v-btn id="crearCuentaBoton" :disabled="!valid" @click="crearCuenta()">CREAR CUENTA</v-btn>
       </div>
     </div>
     <div class="bottom"></div>
@@ -30,30 +45,37 @@
 </template>
 <script>
 //import { functions } from "firebase";
-import firebase from 'firebase';
+//import firebase from 'firebase';
 export default {
   data() {
     return {
+        valid: false,
         correo : "",
         confirmacionCorreo : "",
         password : "",
-        confirmacionPassword : ""
+        confirmacionPassword : "",
+        emailRules: [
+          v => !!v || 'Se necesita un correo electrónico',
+          v => /.+@.+/.test(v) || 'Correo electrónico no válido',
+        ],
+        emailMatch: [
+          v => !!v || 'Se requiere confirmar el correo',
+          () => (this.correo === this.confirmacionCorreo) || 'El correo debe coincidir',
+        ],
+        passwordRules: [
+          v => !!v || 'La contraseña debe tener al menos una minuscula, una mayuscula y un número',
+          v => /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])+/.test(v) || 'Contraseña no válida',
+        ],
+        passwordMatch: [
+          v => !!v || 'Se requiere confirmar la contraseña',
+          (v) => (v === this.password) || 'La contraseña debe coincidir',
+        ],
+
     };
   },
   methods:{
     crearCuenta(){
-      /*let data = {
-        correo: this.correo,
-        password: this.password,
-      };
-      db.collection('usuarios').doc(this.RFC);
-      db.collection('usuarios').doc(this.RFC).set(data);
-      */
-      firebase.auth().createUserWithEmailAndPassword(this.correo, this.password).catch(function(error) {
-        console.log(error.message)
-      });
-      this.$router.replace("/login");
-    }
+      console.log(this.valid);    }
 
 
 
@@ -68,48 +90,56 @@ export default {
 }
 .registroCard {
   position: absolute;
-  top: 20%;
-  transform: translateX(70%);
-  width: 40%;
-  height: 60%;
+  left: 50%;
+  top: 40%;
+  transform: translate(-50%, -50%);
+  width: 40vw;
+  height: 60vh;
 }
 #bienvenidaText {
-  font-size: 24px;
+  font-size: 2.8vh;
   text-align: center;
+  margin-top: 9%;
 }
 #logo {
   position: relative;
-  top: -2%;
+  left: 50%;
+  top: 15%;
+  transform: translate(-50%, -50%);
+  width: 12vh;
+  height: 12vh;
 }
-#textoFields input {
-  display: block;
+#logo img {
+  width: 100%;
+  height: 100%;
+}
+#textoFields{
   position: relative;
-  left: 10%;
-  margin-bottom: 1.8em;
-  width: 40%;
+  left: 50%;
+  top: 42%;
+  transform: translate(-50%, -50%);
+  width: 30vw;
+  height: 50vh;
 }
 #textoFields label {
   float: left;
-  width: 10em;
 }
 #crearCuentaBoton {
   background-color: #1a9ea6;
   color: #ffffff;
-  width: 40%;
-  height: 1%;
+  width: 10em;
+  height: 5vh;
 }
 #buttonWrapper {
   position: relative;
-  bottom: 3%;
+  top: -2vh;
 }
-#logo img {
-  height: 130px;
-}
+
 .top {
   position: absolute;
   background-color: #1a9ea6;
   width: 100%;
-  height: 130px;
+  height: 10vh;
   left: 0px;
   top: 0px;
 }
@@ -117,7 +147,7 @@ export default {
   position: absolute;
   background-color: #1a9ea6;
   width: 100%;
-  height: 130px;
+  height: 10vh;
   left: 0px;
   bottom: 0px;
 }
